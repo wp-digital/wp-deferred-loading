db95fb314d053d813a97f2365fbf0360e2fcda4e
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2020-05-07 14:23:10 +0300

    use media='print' for stylesheet defer

--- src/deferred-loading.php
+++ src/deferred-loading.php
@@ -26,7 +26,11 @@ function is_script_deferred( $handle ) {
  */
 function add_attr_defer( $tag, $handle, $src ) {
     return ! is_admin() && ! is_customize_preview() && is_script_deferred( $handle )
-        ? str_replace( ' src', ' defer onload="' . get_script_onload( $handle, $tag, $src ) . '" src', $tag )
+        ? str_replace(
+                ' src',
+                ' defer onload="' . get_script_onload( $handle, $tag, $src ) . '" src',
+                $tag
+        )
         : $tag;
 }
 
@@ -44,14 +48,15 @@ function get_script_onload( $handle, $tag, $src ) {
 
     $queue = $wp_scripts->queue;
 
-    return apply_filters( 'deferred_loading_script_onload',
-        wp_script_is( 'jquery' )
-        && is_script_deferred( 'jquery-core' )
-        && end( $queue ) === $handle
+    return apply_filters(
+        'deferred_loading_script_onload',
+        wp_script_is( 'jquery' ) && is_script_deferred( 'jquery-core' ) && end( $queue ) === $handle
             ? justify_jquery()
             : '',
-        $handle, $tag, $src );
-
+        $handle,
+        $tag,
+        $src
+    );
 }
 
 add_filter( 'script_loader_tag', __NAMESPACE__ . '\add_attr_defer', 10, 3 );
@@ -101,7 +106,11 @@ function has_deferred_styles() {
 
     $handles = apply_filters( 'deferred_loading_styles', [] );
 
-    return ! empty( $handles ) && ( $handles == '*' || count( array_intersect( $handles, array_keys( $wp_styles->registered ) ) ) > 0 );
+    return ! empty( $handles ) &&
+        (
+            $handles == '*' ||
+            count( array_intersect( $handles, array_keys( $wp_styles->registered ) ) ) > 0
+        );
 }
 
 /**
@@ -109,27 +118,19 @@ function has_deferred_styles() {
  *
  * @param string $tag
  * @param string $handle
+ * @param string $href
+ * @param string $media
  *
  * @return string
  */
-function add_attr_rel_preload( $tag, $handle ) {
-    return ! is_admin() && ! is_customize_preview() && is_style_deferred( $handle )
-        ? str_replace( ' rel=\'stylesheet\'', ' rel=\'preload\' as=\'style\' onload=\'this.rel="stylesheet"\'', $tag ) . "<noscript>$tag</noscript>"
+function add_attr_rel_preload( $tag, $handle, $href, $media ) {
+    return ! is_admin() && ! is_customize_preview() && $media != 'print' && is_style_deferred( $handle )
+        ? str_replace(
+                " media='$media'",
+                " media='print' onload='this.media=\"$media\";this.onload=null;'",
+                $tag
+        ) . "<noscript>\n$tag</noscript>\n"
         : $tag;
 }
 
-add_filter( 'style_loader_tag', __NAMESPACE__ . '\add_attr_rel_preload', 10, 2 );
-
-/**
- * Prints loadCSS.js with 'preload' polyfill
- */
-function load_css_script() {
-    if ( has_deferred_styles() ) : ?>
-        <script>
-          !function(e){"use strict";var n=function(n,t,o){function i(e){return a.body?e():void setTimeout(function(){i(e)})}function r(){l.addEventListener&&l.removeEventListener("load",r),l.media=o||"all"}var d,a=e.document,l=a.createElement("link");if(t)d=t;else{var s=(a.body||a.getElementsByTagName("head")[0]).childNodes;d=s[s.length-1]}var f=a.styleSheets;l.rel="stylesheet",l.href=n,l.media="only x",i(function(){d.parentNode.insertBefore(l,t?d:d.nextSibling)});var u=function(e){for(var n=l.href,t=f.length;t--;)if(f[t].href===n)return e();setTimeout(function(){u(e)})};return l.addEventListener&&l.addEventListener("load",r),l.onloadcssdefined=u,u(r),l};"undefined"!=typeof exports?exports.loadCSS=n:e.loadCSS=n}("undefined"!=typeof global?global:this);
-          !function(t){if(t.loadCSS){var e=loadCSS.relpreload={};if(e.support=function(){try{return t.document.createElement("link").relList.supports("preload")}catch(e){return!1}},e.poly=function(){for(var e=t.document.getElementsByTagName("link"),n=0;n<e.length;n++){var r=e[n];"preload"===r.rel&&"style"===r.getAttribute("as")&&(t.loadCSS(r.href,r),r.rel=null)}},!e.support()){e.poly();var n=t.setInterval(e.poly,300);t.addEventListener&&t.addEventListener("load",function(){t.clearInterval(n)}),t.attachEvent&&t.attachEvent("onload",function(){t.clearInterval(n)})}}}(this);
-        </script>
-    <?php endif;
-}
-
-add_filter( 'wp_head', __NAMESPACE__ . '\load_css_script', 99 );
+add_filter( 'style_loader_tag', __NAMESPACE__ . '\add_attr_rel_preload', 10, 4 );
--- wp-deferred-loading.php
+++ wp-deferred-loading.php
@@ -2,11 +2,11 @@
 /**
  * Plugin Name: Deferred loading
  * Description: Defer loading of JavaScript and CSS files.
- * Version: 1.2.0
+ * Version: 1.3.0
  * Author: Innocode
  * Author URI: https://innocode.com
  * Requires at least: 4.8
- * Tested up to: 5.2.2
+ * Tested up to: 5.4.1
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
